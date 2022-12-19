062e2004e376f8d2880cd6377681912eeb3eb05a
Valerii96 <shilenko.valera2019@gmail.com>
2022-12-19 22:24:27 +0200

    added top-slider section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
 $(function (){
-
+	$('.top-slider__wrapper').slick({
+		arrows: false,
+		dots: true,
+		fade: true,
+		autoplay: true,
+  	autoplaySpeed: 2000,
+		dotsClass: 'top-slider__dots'
+	})
 });
