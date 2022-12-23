776d64f1e0d1d4ba721885f08b02f4ec9adbbe7e
Valerii96 <shilenko.valera2019@gmail.com>
2022-12-23 16:33:04 +0200

    added function sort product

--- app/js/main.js
+++ app/js/main.js
@@ -6,5 +6,26 @@ $(function (){
 		autoplay: true,
   	autoplaySpeed: 2000,
 		dotsClass: 'top-slider__dots'
-	})
+	});
+
+
+
+
+var containerEl1 = document.querySelector('[data-ref="container-1"]');
+var containerEl2 = document.querySelector('[data-ref="container-2"]');
+
+var config = {
+	controls: {
+		scope: 'local'
+	}
+};
+
+var mixer1 = mixitup(containerEl1, config);
+var mixer2 = mixitup(containerEl2, config);
+
+
+
+
 });
+
+
